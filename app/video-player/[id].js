07cbde91e78285b925ddd3a4f86fd16e4fc3c3f9
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-04-15 15:15:41 +0300

    fixed video player

--- app/video-player/[id].js
+++ app/video-player/[id].js
@@ -1,27 +1,22 @@
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { SafeAreaView, View } from "react-native";
-import YoutubePlayer from "react-native-youtube-iframe";
+// import YoutubePlayer from "react-native-youtube-iframe";
 import useFetch from "../../hooks/useFetch";
 import styles from "./videoplayer.style";
 
 const VideoPlayerPage = () => {
     const params = useSearchParams();
     const router = useRouter();
-    // const video = useRef(null);
+    let trailer = ""
 
     const { data, isLoading, error, refetch } = useFetch(`movie/${params.id}/videos`, {
         api_key: "8e325dd835257b2af80167acb9002318",
     });
-    // if (data.results)
-        // console.log(`https://www.youtube.com/watch?v=${data?.results[0].key}`)
+
+    if(data.results)
+        trailer = data.results.find(i => i.type === "Trailer").key
+
     return (
-        // <View>
-        //     <YoutubePlayer
-        //         height={300}
-        //         play={true}
-        //         videoId={"imSefM4GPpE"}
-        //     />
-        // </View>
         <SafeAreaView style={styles.container}>
             <Stack.Screen
                 options={{
@@ -33,11 +28,11 @@ const VideoPlayerPage = () => {
             />
 
             <View>
-                <YoutubePlayer
+                {/* <YoutubePlayer
                     height={300}
                     play={false}
-                    videoId={data?.results[0].key}
-                />
+                    videoId={trailer}
+                /> */}
             </View>
         </SafeAreaView>
     )
