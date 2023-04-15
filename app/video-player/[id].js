071198e426d1a28b78011821a1f586cc3e589e4f
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-04-15 15:32:24 +0300

    added trailer name

--- app/video-player/[id].js
+++ app/video-player/[id].js
@@ -1,20 +1,20 @@
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { SafeAreaView, View } from "react-native";
-// import YoutubePlayer from "react-native-youtube-iframe";
+import YoutubePlayer from "react-native-youtube-iframe";
 import useFetch from "../../hooks/useFetch";
 import styles from "./videoplayer.style";
 
 const VideoPlayerPage = () => {
     const params = useSearchParams();
     const router = useRouter();
-    let trailer = ""
+    let trailer = null
 
     const { data, isLoading, error, refetch } = useFetch(`movie/${params.id}/videos`, {
         api_key: "8e325dd835257b2af80167acb9002318",
     });
 
     if(data.results)
-        trailer = data.results.find(i => i.type === "Trailer").key
+        trailer = data.results.find(i => i.type === "Trailer")
 
     return (
         <SafeAreaView style={styles.container}>
@@ -22,17 +22,17 @@ const VideoPlayerPage = () => {
                 options={{
                     headerStyle: { backgroundColor: 'lightgray' },
                     headerShadowVisible: false,
-                    headerTitle: data?.title,
+                    headerTitle: trailer?.name,
                     headerTitleAlign: 'center'
                 }}
             />
 
             <View>
-                {/* <YoutubePlayer
+                <YoutubePlayer
                     height={300}
                     play={false}
                     videoId={trailer}
-                /> */}
+                />
             </View>
         </SafeAreaView>
     )
