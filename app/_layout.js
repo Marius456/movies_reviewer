a31bddee2c8ba3dc754756a48d3e6c087c241259
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-04-18 10:49:38 +0300

    fixed initial route

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,9 @@
 import { Stack } from "expo-router";
 
-export const unstable_settings = {
-    initialRouteName: "home",
-  };
-
 const Layout = () => {
     return (
-
-        <Stack initialRouteName="home">
-            <Stack.Screen name="home" />
+        <Stack initialRouteName="home/home">
+            <Stack.Screen name="home/home" />
         </Stack>
     )
 }
